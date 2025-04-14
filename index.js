5a3c30ffea85336b0bb28b3c5a8171eb1e1cb9fa
Krishcode264 <krishnazade99@gmail.com>
2025-04-14 08:12:03 +0530

    delay

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 const http = require("http");
 
-
-
-const keepALive=["https://api.socialsphere.krishcode264.online/health","https://api.boldhug.krishcode264.online"]
+const keepALive = [
+  "https://api.socialsphere.krishcode264.online",
+  "https://api.boldhug.krishcode264.online",
+];
 const sendInterval = () => {
-    setInterval(async () => {
-         keepALive.forEach(async(url)=>{
-            console.log("sending req")
-            await fetch(url,{method:"GET"})
-         })
-    }, 1000 *60*8);
-  };
+  setInterval(async () => {
+    keepALive.forEach(async (url) => {
+      console.log("sending req");
+      await fetch(url, {
+        method: "GET",
+        headers: {
+          "User-Agent": "Mozilla/5.0",
+        },
+      });
+    });
+  }, 1000 * 60 * 2);
+};
 
-  const server = http.createServer((req, res) => {
-    res.writeHead(200);
-    res.end("Keep-alive server running.");
-  });
-  
-  server.listen(8080, () => {
-    console.log("Keep-alive server started on port 8080.");
-    sendInterval();
-  });
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end("Keep-alive server running.");
+});
+
+server.listen(8080, () => {
+  console.log("Keep-alive server started on port 8080.");
+  sendInterval();
+});
