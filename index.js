659fd8f19eeb85f7df765ae2f50a5bfbfb9399e3
Krishcode264 <krishnazade99@gmail.com>
2025-04-14 07:40:37 +0530

    updated

--- index.js
+++ index.js
@@ -1,24 +1,10 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  const { url, method } = req;
-  const parsedUrl = url.parse(req.url, true);
-  const pathname = parsedUrl.pathname;
-  const query = parsedUrl.query;
-
-  sendafterDelay(query.url);
-  if (pathname === "/bealive") {
-    console.log("being alive");
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("being alive ");
-  }
+ 
 });
 
-const sendafterDelay = (url) => {
-  setTimeout(async () => {
-    await fetch(url, { method: "GET" });
-  }, 1000 * 60 * 5);
-};
+
 
 const keepALive=["https://api.socialsphere.krishcode264.online/health","https://api.boldhug.krishcode264.online"]
 const sendInterval = () => {
